9419549073e4da1604e5a43717694fddf8dd0b38
paulave <101834181+paulave@users.noreply.github.com>
2022-03-30 18:08:06 -0300

    Add files via upload

---
+++ TP3-2.js
@@ -0,0 +1,47 @@
+var LadoARectangulo=20
+var ladoBRectangulo=10
+console.log("La superficie del rectangulo es " + LadoARectangulo*ladoBRectangulo + " cm")
+
+var BaseTriangulo= 15
+var AlturaTriangulo= 30
+console.log("La superficie del triangulo es " + (BaseTriangulo*AlturaTriangulo)/2 + " cm")
+
+const Pi= 3.14
+var radioCirculo= 13
+console.log("La superficie del circulo es " + Pi*(radioCirculo*radioCirculo) + " cm")
+
+var num1= 3
+var num2= 15
+var num3= 27
+var Producto = num1*num2*num3
+console.log("El producto es " + Producto)
+
+var suma= num1+num2+num3
+console.log("La suma es " + suma)
+
+var promedio= (num1+num2+num3)/3
+console.log("El promedio es " + promedio)
+
+var Anillo= 1000
+var Pulsera = 1500
+var Collar= 2500
+var Aros= 1700
+var Hebilla= 500
+var Compra= Anillo+Pulsera+Collar+Aros+Hebilla
+var Descuento= Compra*0.15
+
+console.log("El descuento es de " + Descuento + " pesos")
+console.log("El valor de final de la compra es " + (Compra-Descuento) + " pesos")
+
+var Pesos = 10000
+var Dolar = 100
+
+console.log("La equivalencia en dolares es " + Pesos/Dolar)
+
+var compratotal= 50000
+const IVA= 0.21
+var MontoIVA= compratotal*IVA
+console.log(" La compra total es de " + compratotal + " pesos. Y el IVA ya incluido es " + MontoIVA + " pesos")
+
+
+
